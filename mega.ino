c656cb0127d48fdc0b804cc3ee956adc078ad779
Francis Regan <francis_regan@yahoo.co.in>
2020-07-04 19:40:30 +0800

    Revision #12

---
+++ mega.ino
@@ -0,0 +1,32 @@
+const int PIR_1 = A0;
+const int PIR_2 = A1;
+const int PIR_3 = A2;
+const int PIR_4 = A3;
+const int PIR_5 = A4;
+
+void setup() {
+  Serial2.begin(19200);
+  Serial.begin(9600);
+}
+
+
+void loop() {
+
+  float pir1;  
+  float pir2;
+  float pir3;
+  float pir4;
+  float pir5;
+
+  pir1 = analogRead(PIR_1) * (5.0 / 1023.0);
+  pir2 = analogRead(PIR_2) * (5.0 / 1023.0);
+  pir3 = analogRead(PIR_3) * (5.0 / 1023.0);
+  pir4 = analogRead(PIR_4) * (5.0 / 1023.0);
+  pir5 = analogRead(PIR_5) * (5.0 / 1023.0);
+
+  String serialData = String(pir1) + "," + String(pir2) + "," + String(pir3) + "," + String(pir4) + "," + String(pir5);
+
+  Serial2.println(serialData);
+  Serial.println(serialData);
+}
+
