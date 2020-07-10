262ad345606eeea97adc7c512a65b9765ba4ddf0
Tridev786 <59784238+Tridev786@users.noreply.github.com>
2020-07-10 13:59:34 +0530

    Update mega.ino

--- mega.ino
+++ mega.ino
@@ -1,15 +1,24 @@
+// Message, call PIR
 const int PIR_1 = A0;
 const int PIR_2 = A1;
 const int PIR_3 = A2;
 const int PIR_4 = A3;
 const int PIR_5 = A4;
+
+// Camera PIR
 const int PIR_6 = A5;
 const int PIR_7 = A6;
 const int PIR_8 = A7;
 const int PIR_9 = A8;
 const int PIR_10 = A9;
+
+// Fire
 const int PIR_11 = A10;
+
+//Alert
 const int PIR_12 = A11;
+
+// Emergency
 const int PIR_13 = A12;
 
 void setup() {
@@ -17,33 +26,43 @@ void setup() {
   Serial.begin(9600);
 }
 
-
 void loop() {
 
-  float pir1 = 0.0;  
-  float pir2 = 0.0;
-  float pir3 = 0.0;
-  float pir4 = 0.0;
-  float pir5 = 0.0;
-  float pir11 = 0.0;
-  float pir12 = 0.0;
-  float pir13 = 0.0;
+ byte p1=0,p2=0,p3=0,p4=0,p5=0,p11=0,p12=0,p13 = 0; 
 
-  pir1 = analogRead(PIR_1) * (5.0 / 1023.0);
-  pir2 = analogRead(PIR_2) * (5.0 / 1023.0);
-  pir3 = analogRead(PIR_3) * (5.0 / 1023.0);
-  pir4 = analogRead(PIR_4) * (5.0 / 1023.0);
-  pir5 = analogRead(PIR_5) * (5.0 / 1023.0);
-  pir11 = analogRead(PIR_11) * (5.0 / 1023.0);
-  pir12 = analogRead(PIR_12) * (5.0 / 1023.0);
-  pir13 = analogRead(PIR_13) * (5.0 / 1023.0);
+ float pir1 = analogRead(PIR_1) * (5.0 / 1023.0);
+ float pir2 = analogRead(PIR_2) * (5.0 / 1023.0);
+ float pir3 = analogRead(PIR_3) * (5.0 / 1023.0);
+ float pir4 = analogRead(PIR_4) * (5.0 / 1023.0);
+ float pir5 = analogRead(PIR_5) * (5.0 / 1023.0);
+ float pir11 = analogRead(PIR_11) * (5.0 / 1023.0);
+ float pir12 = analogRead(PIR_12) * (5.0 / 1023.0);
+ float pir13 = analogRead(PIR_13) * (5.0 / 1023.0);
+ 
+  if(pir1>=4.9){
+    p1 = 1;
+  }else if(pir2>=4.9){
+    p2 = 1;
+  }else if(pir3>=4.9){
+    p3 = 1;
+  }else if(pir4>=4.9){
+    p4 = 1;
+  }else if(pir5>=4.9){
+    p5 = 1;
+  }else if(pir11>=4.9){
+    p11 = 1;
+  }else if(pir12>=4.9){
+    p12 = 1;
+  }else if(pir13>=4.9){
+    p13 = 1;
+  }
 
-  String serialData = String(pir1) + "," + String(pir2) + "," + String(pir3) 
-                      + "," + String(pir4) + "," + String(pir5) + "," + String(pir11)
-                      + "," + String(pir12) + "," + String(pir13);
+  String serialData = String(p1) + "," + String(p2) + "," + String(p3) 
+                      + "," + String(p4) + "," + String(p5) + "," + String(p11)
+                      + "," + String(p12) + "," + String(p13);
 
   Serial2.println(serialData);
   Serial.println(serialData);
-  delay(1000);
+  delay(2000);
 }
 
